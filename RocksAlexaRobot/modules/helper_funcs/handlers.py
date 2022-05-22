5875842f4d0dd91e46933de4dd77fa7cd633dda6
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-05-22 20:20:38 +0500

    Alexa 3.0

--- RocksAlexaRobot/modules/helper_funcs/handlers.py
+++ RocksAlexaRobot/modules/helper_funcs/handlers.py
@@ -1,31 +1,33 @@
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
-# Without Credit (Mother Fucker)
-# Rocks © @Dr_Asad_Ali © Rocks
-# Owner Asad + Harshit
-
-
 import RocksAlexaRobot.modules.sql.blacklistusers_sql as sql
 from RocksAlexaRobot import ALLOW_EXCL
-from RocksAlexaRobot import (DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES)
+from RocksAlexaRobot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
 
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
-from pyrate_limiter import (BucketFullException, Duration, RequestRate, Limiter,
-                            MemoryListBucket)
+from pyrate_limiter import (
+    BucketFullException,
+    Duration,
+    RequestRate,
+    Limiter,
+    MemoryListBucket,
+)
 
 if ALLOW_EXCL:
-    CMD_STARTERS = ('/', '!', '?', '.', '~')
+    CMD_STARTERS = ("/", "!", "~")
 else:
-    CMD_STARTERS = ('/',)
+    CMD_STARTERS = ("/", "!", "~",)
 
 
 class AntiSpam:
-
     def __init__(self):
-        self.whitelist = (DEV_USERS or []) + (DRAGONS or []) + (
-            WOLVES or []) + (DEMONS or []) + (
-                TIGERS or [])
-        #Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
+        self.whitelist = (
+            (DEV_USERS or [])
+            + (DRAGONS or [])
+            + (WOLVES or [])
+            + (DEMONS or [])
+            + (TIGERS or [])
+        )
+        # Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
         Duration.CUSTOM = 15  # Custom duration, 15 seconds
         self.sec_limit = RequestRate(6, Duration.CUSTOM)  # 6 / Per 15 Seconds
         self.min_limit = RequestRate(20, Duration.MINUTE)  # 20 / Per minute
@@ -36,7 +38,8 @@ class AntiSpam:
             self.min_limit,
             self.hour_limit,
             self.daily_limit,
-            bucket_class=MemoryListBucket)
+            bucket_class=MemoryListBucket,
+        )
 
     def check_user(self, user):
         """
@@ -56,19 +59,13 @@ MessageHandlerChecker = AntiSpam()
 
 
 class CustomCommandHandler(CommandHandler):
-
-    def __init__(self,
-                 command,
-                 callback,
-                 admin_ok=False,
-                 allow_edit=False,
-                 **kwargs):
+    def __init__(self, command, callback, admin_ok=False, allow_edit=False, **kwargs):
         super().__init__(command, callback, **kwargs)
 
         if allow_edit is False:
             self.filters &= ~(
-                Filters.update.edited_message
-                | Filters.update.edited_channel_post)
+                Filters.update.edited_message | Filters.update.edited_channel_post
+            )
 
     def check_update(self, update):
         if isinstance(update, Update) and update.effective_message:
@@ -79,43 +76,40 @@ class CustomCommandHandler(CommandHandler):
             except:
                 user_id = None
 
-            if user_id:
-                if sql.is_user_blacklisted(user_id):
-                    return False
+            if user_id and sql.is_user_blacklisted(user_id):
+                return False
 
             if message.text and len(message.text) > 1:
                 fst_word = message.text.split(None, 1)[0]
                 if len(fst_word) > 1 and any(
-                        fst_word.startswith(start) for start in CMD_STARTERS):
+                    fst_word.startswith(start) for start in CMD_STARTERS
+                ):
 
                     args = message.text.split()[1:]
                     command = fst_word[1:].split("@")
                     command.append(message.bot.username)
                     if user_id == 1087968824:
                         user_id = update.effective_chat.id
-                    if not (command[0].lower() in self.command and
-                            command[1].lower() == message.bot.username.lower()):
+                    if not (
+                        command[0].lower() in self.command
+                        and command[1].lower() == message.bot.username.lower()
+                    ):
                         return None
                     if SpamChecker.check_user(user_id):
                         return None
                     filter_result = self.filters(update)
                     if filter_result:
                         return args, filter_result
-                    else:
-                        return False
+                    return False
 
     def handle_update(self, update, dispatcher, check_result, context=None):
         if context:
-            self.collect_additional_context(context, update, dispatcher,
-                                            check_result)
+            self.collect_additional_context(context, update, dispatcher, check_result)
             return self.callback(update, context)
-        else:
-            optional_args = self.collect_optional_args(dispatcher, update,
-                                                       check_result)
-            return self.callback(dispatcher.bot, update, **optional_args)
+        optional_args = self.collect_optional_args(dispatcher, update, check_result)
+        return self.callback(dispatcher.bot, update, **optional_args)
 
-    def collect_additional_context(self, context, update, dispatcher,
-                                   check_result):
+    def collect_additional_context(self, context, update, dispatcher, check_result):
         if isinstance(check_result, bool):
             context.args = update.effective_message.text.split()[1:]
         else:
@@ -125,29 +119,18 @@ class CustomCommandHandler(CommandHandler):
 
 
 class CustomRegexHandler(RegexHandler):
-
     def __init__(self, pattern, callback, friendly="", **kwargs):
         super().__init__(pattern, callback, **kwargs)
 
 
 class CustomMessageHandler(MessageHandler):
-
-    def __init__(self,
-                 filters,
-                 callback,
-                 friendly="",
-                 allow_edit=False,
-                 **kwargs):
+    def __init__(self, filters, callback, friendly="", allow_edit=False, **kwargs):
         super().__init__(filters, callback, **kwargs)
         if allow_edit is False:
             self.filters &= ~(
-                Filters.update.edited_message
-                | Filters.update.edited_channel_post)
+                Filters.update.edited_message | Filters.update.edited_channel_post
+            )
 
         def check_update(self, update):
             if isinstance(update, Update) and update.effective_message:
                 return self.filters(update)
-              
-              #done
-              
-# Roses are red, Violets are blue, A face like yours, Belongs in a zoo
